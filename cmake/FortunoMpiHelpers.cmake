4bf2481a8cc7ea64fc91d69757010154d6c89d58
Bálint Aradi <aradi@uni-bremen.de>
2024-03-11 21:01:31 +0100

    Implement a cleaner approach for subprojects in CMake (#2)

--- cmake/FortunoMpiHelpers.cmake
+++ cmake/FortunoMpiHelpers.cmake
@@ -1,4 +1,6 @@
-include(FetchContent)
+# This file is part of Fortuno.
+# Licensed under the BSD-2-Clause Plus Patent license.
+# SPDX-License-Identifier: BSD-2-Clause-Patent
 
 
 # Sets up the build type.
@@ -16,85 +18,8 @@ function (fortuno_mpi_setup_build_type default_build_type)
         CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "RelWithDebInfo"
       )
     else()
-      message(STATUS "Fortuno: build type: ${CMAKE_BUILD_TYPE} (manually selected single-config)")
+      message(STATUS "Build type ${CMAKE_BUILD_TYPE} (manually selected single-config)")
     endif()
   endif()
 
 endfunction()
-
-
-# Gets a subproject.
-#
-# Args:
-#   subproject: name of the subproject
-#   get_methods: priority list of get methods to obtain the subproject,
-#     possible choices are 'find' and 'fetch'
-#
-macro (fortuno_mpi_get_subproject subproject get_methods)
-
-  set(_prefix fortuno_mpi_sub_${subproject})
-  set(_subproject_package ${${_prefix}_package})
-  set(_subproject_source_dir ${${_prefix}_source_dir})
-  set(_subproject_target ${${_prefix}_target})
-  set(_subproject_git_repository ${${_prefix}_git_repository})
-  set(_subproject_git_revision ${${_prefix}_git_revision})
-  set(_subproject_dir ${${_prefix}_source_dir})
-
-  if (TARGET ${_subproject_target})
-    return ()
-  endif ()
-
-  foreach(_get_method IN ITEMS ${get_methods})
-
-    if ("${_get_method}" STREQUAL "find")
-      find_package(${_subproject_package})
-      if (${_subproject_package}_FOUND)
-        message(STATUS "Subproject ${subproject}: package ${_subproject_package} found")
-        break()
-      else ()
-        message(STATUS "Subproject ${subproject}: package ${_subproject_package} not found")
-      endif ()
-    elseif ("${_get_method}" STREQUAL "fetch")
-      if (EXISTS ${_subproject_dir})
-        message(
-          STATUS
-          "Subproject ${subproject}: fetch skipped as source folder ${_subproject_dir} exists "
-          "already"
-        )
-      else ()
-        message(
-          STATUS
-          "Subproject ${subproject}: fetching from repository ${_subproject_git_repository} "
-          "(revision ${_subproject_git_revision}) into source folder ${_subproject_dir}"
-        )
-        FetchContent_Declare(
-          ${subproject}
-          SOURCE_DIR ${_subproject_dir}
-          GIT_REPOSITORY ${_subproject_git_repository}
-          GIT_TAG ${_subproject_git_revision}
-        )
-        FetchContent_Populate(${subproject})
-      endif ()
-      cmake_language(CALL ${_prefix}_add_subdir)
-      break()
-    endif ()
-
-  endforeach ()
-
-  if (NOT TARGET ${_subproject_target})
-    message(
-      FATAL_ERROR
-      "Subproject ${subproject}: Could not obtain to export target ${_subproject_target}"
-    )
-  endif ()
-
-  unset(_prefix)
-  unset(_subproject_package)
-  unset(_subproject_source_dir)
-  unset(_subproject_target)
-  unset(_subproject_git_repository)
-  unset(_subproject_git_revision)
-  unset(_subproject_dir)
-  unset(_get_method)
-
-endmacro ()
--- subprojects/fortuno.cmake
+++ subprojects/fortuno.cmake
@@ -1,14 +1,30 @@
-set(fortuno_mpi_sub_fortuno_package Fortuno)
-set(fortuno_mpi_sub_fortuno_source_dir ${PROJECT_SOURCE_DIR}/subprojects/fortuno)
-set(fortuno_mpi_sub_fortuno_target Fortuno::Fortuno)
-set(fortuno_mpi_sub_fortuno_git_repository "https://github.com/fortuno-repos/fortuno.git")
-set(fortuno_mpi_sub_fortuno_git_revision "main")
+# Imports the Fortuno subproject.
 
-function (fortuno_mpi_sub_fortuno_add_subdir)
-  set(FORTUNO_INSTALL ${FORTUNO_MPI_INSTALL})
-  set(FORTUNO_INSTALL_MODULEDIR ${FORTUNO_MPI_INSTALL_MODULEDIR})
-  set(FORTUNO_BUILD_SHARED_LIBS ${FORTUNO_MPI_BUILD_SHARED_LIBS})
-  add_subdirectory(subprojects/fortuno)
-endfunction ()
+# Define variables influencing how and from where the subproject is obtained
+set(CMAKE_REQUIRE_FIND_PACKAGE_Fortuno ${FORTUNO_MPI_SUBPROJECT_REQUIRE_FIND})
+set(CMAKE_DISABLE_FIND_PACKAGE_Fortuno ${FORTUNO_MPI_SUBPROJECT_DISABLE_FIND})
+# set FETCHCONTENT_SOURCE_DIR_FORTUNO to use a local source of the subproject
 
-fortuno_mpi_get_subproject(fortuno "${FORTUNO_MPI_SUBPROJECT_GET_METHODS}")
+# Define all subproject related variables
+option(FORTUNO_INSTALL "Fortuno: Whether to install project" ${FORTUNO_MPI_INSTALL})
+set(
+  FORTUNO_INSTALL_MODULEDIR "${FORTUNO_MPI_INSTALL_MODULEDIR}" CACHE STRING
+  "Fortuno: Installation directory for Fortran module files (relative to CMAKE_INSTALL_LIBDIR)"
+)
+option(
+  FORTUNO_BUILD_SHARED_LIBS "Fortuno: Build as shared library" ${FORTUNO_MPI_BUILD_SHARED_LIBS}
+)
+
+FetchContent_Declare(
+  Fortuno
+  GIT_REPOSITORY "https://github.com/fortuno-repos/fortuno.git"
+  GIT_TAG "main"
+  FIND_PACKAGE_ARGS
+)
+FetchContent_MakeAvailable(Fortuno)
+
+if (Fortuno_FOUND)
+  message(STATUS "Subproject Fortuno: using installed version")
+else ()
+  message(STATUS "Subproject Fortuno: building from source in ${fortuno_SOURCE_DIR}")
+endif ()
